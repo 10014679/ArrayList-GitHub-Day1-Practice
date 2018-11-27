810e348e3401b89eedd767bfb1ff93c925b7f6b5
Skyla Qian <10014679@sbstudents.org>
2018-11-27 12:32:26 -0500

    Filling My ArrayLists
    I am filling my ArrayLists. Then, I am adding the elements of my 2 ArrayLists to my totallist.

--- GitHubDay1.java
+++ GitHubDay1.java
@@ -5,6 +5,15 @@ public class GitHubDay1{
 	ArrayList<Integer> list1 = new ArrayList<>();
 	ArrayList<Integer> list2 = new ArrayList<>();
 	ArrayList<Integer> totallist = new ArrayList<>();
+	for(int i = 0; i < 10; i++){
+			list1.add(i);
+			list2.add(i);
+	}
+
+	for(int i = 0; i < 10; i++){
+		totallist.add(list1.get(i));
+		totallist.add(list2.get(i));
+	}
 
 	}
 }
